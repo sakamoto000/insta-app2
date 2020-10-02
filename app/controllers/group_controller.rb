e0d8c0cfefece4952c4d3200fdb1edebc812506c
さかもと <>
2020-10-02 16:27:43 +0900

    devise

---
+++ app/controllers/group_controller.rb
@@ -0,0 +1,11 @@
+class GroupsController < ApplicationController
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      flash.now[:alert] = "グループ名を入力してください"
+      render :new
+    end
+  end
+end
