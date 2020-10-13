bf0e56452b8336e02c2a80cbc4fa403ec026d58c
さかもと <>
2020-10-13 22:33:10 +0900

    プロフィール完成（仮）

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,5 @@
 class Profile < ApplicationRecord
+  enum gender: { male: 0, female: 1, other: 2 }
   belongs_to :user
+  has_one_attached :user_image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name,
     uniqueness: { case_sensitive: :false }
-  
+
   has_one :profile, dependent: :destroy
+
+  def prepare_profile
+    profile || build_profile
+  end
 end
