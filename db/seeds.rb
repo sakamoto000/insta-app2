ec0db23458f6d6af1e2b6043cb2d1fd24a8912e0
さかもと <>
2020-11-05 21:53:42 +0900

    記事投稿、編集、削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
-  Article.create(
-    content: Faker::Lorem.sentence(word_count: 5)
-  )
-end
+# 10.times do
+#   Article.create(
+#     content: Faker::Lorem.sentence(word_count: 5)
+#   )
+# end
